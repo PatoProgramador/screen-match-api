a9cb3bcc044a64c5430682306f5db6a0137204a7
PatoProgramador <skpates18@gmail.com>
2024-04-13 12:19:17 -0500

    Data Types and variable declaration

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenidx a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDePelicula = 8.2;
+
+        double media = (notaDePelicula + 6.0 + 9.0) / 3;
+        System.out.println(media);
     }
 }
