ae3d2cc3eda8e2130242da1f043470ac7d22ee55
PatoProgramador <skpates18@gmail.com>
2024-04-13 13:12:44 -0500

    Logical operatos

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -1,13 +1,20 @@
 public class Decisiones {
     public static void main(String[] args) {
         int fechaDeLanzamiento = 1999;
-        boolean incluidoEnElPlan = true;
+        boolean incluidoEnElPlan = false;
         double notaDePelicula = 8.2;
+        String tipoPlan = "plus";
 
         if (fechaDeLanzamiento >= 2022) {
             System.out.println("Peliculas más nuevas");
         } else {
             System.out.println("Peliculas Retro que son shidas");
+        }
+
+        if (incluidoEnElPlan && tipoPlan.equals("plus")) {
+            System.out.println("Disfrute de su pelicula");
+        } else {
+            System.out.println("Pelicula no disponible para su tipo de plan actual");
         }
     }
 }
